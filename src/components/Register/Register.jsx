a47bfa0f5a084d3092a57e51f205aa0da13d5a8f
itIsMuse <oyeniranshock@gmail.com>
2025-01-04 06:32:56 +0100

    removed the div

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -51,7 +51,6 @@ const sendInfo = () => {
  
   }
   return (
-    <div>
 <form className="max-w-sm mx-auto shadow-xl">
   <div className='mx-10 mb-5'>
   <div className="mb-5">
@@ -69,7 +68,7 @@ const sendInfo = () => {
   <button onClick={sendInfo} type="submit" className=" mb-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
 </div>
 </form>
-    </div>
+
   )
 }
 
